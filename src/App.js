bd18f57b2d7f01769fc7cca43932408f21f1fa11
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-10 23:36:11 +0530

    ADDED: display hello world using one way data bindings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      title:"hello from bridgelabz"
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      <h1>{this.state.title}</h1>
+        </div>
+    );
+  }
 }
 
 export default App;
