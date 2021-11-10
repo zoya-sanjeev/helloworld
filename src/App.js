4c42cdf530353da3d908217f14706f40126a4ed7
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-10 23:47:39 +0530

    ADDED: bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './BridgeLabz.jpg';
 import './App.css';
 import React from 'react';
 
@@ -14,6 +14,7 @@ class App extends React.Component{
     return(
       <div>
       <h1>{this.state.title}</h1>
+      <img src={logo} alt="loaded"/>
         </div>
     );
   }
